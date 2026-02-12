4b4b6c9862f82e5bca0d9609cb4522b8bc870ef8
puliwebdev <puliweb.dev@gmail.com>
2026-02-12 12:03:07 -0300

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,145 @@
+const juegos = [
+    {
+        id: 1,
+        nombre: "resident evil 4",
+        descripcion: "terror, Supervivencia, accion",
+        descripcionLarga: "Descargar Resident Evil 4 Remake para PC en Español.",
+        imagen: "https://image.api.playstation.com/vulcan/ap/rnd/202210/0712/BiS5QP6h4506JHyJlZlVzK9D.jpg",
+        descarga: "https://playpaste.net/?v=jPif",
+        requisitos: {
+        minimo: [
+        "Requiere un procesador y un sistema operativo de 64 bits",
+        "SO: Windows 10 (64 bit)",
+        "PROCESADOR: AMD Ryzen 3 1200 / Intel Core i5-7500",
+        "MEMORIA: 8 GB de RAM",
+        "GRÁFICOS: AMD Radeon RX 560 with 4GB VRAM / NVIDIA GeForce GTX 1050 Ti with 4GB VRAM",
+        "DIRECTX: Versión 12",
+        ],
+        recomendado: [
+        "Requiere un procesador y un sistema operativo de 64 bits",
+        "SO: Windows 10 (64 bit)/Windows 11 (64 bit)",
+        "PROCESADOR: AMD Ryzen 5 3600 / Intel Core i7 8700",
+        "MEMORIA: 16 GB de RAM",
+        "GRÁFICOS: AMD Radeon RX 5700 / NVIDIA GeForce GTX 1070",
+        "DIRECTX: Versión 12",
+        
+        ]
+        },
+
+        contenido: `
+        Descargar Resident Evil 4 Remake Deluxe Edition para PC en Español v03.02.2026.
+
+        Incluye:
+        - DLC Separate Ways
+        - The Mercenaries
+        - Treasure Map Expansion
+        - Soundtrack
+        - Leon & Ashley Costumes (Casual y Romantic)
+        - Filtros Hero & Villain
+        - Deluxe Weapons
+        - Accesorios
+        - Exclusive Upgrade Ticket x11
+        `,
+
+        tutoriales: [
+        {
+        titulo: "Cómo instalar el juego",
+        link: "https://playpaste.net/?v=jPiU"
+        },
+
+        ]
+        
+    },
+    {
+        id: 2,
+        nombre: "silent hill 2 REMAKE",
+        descripcion: "Terror psicologico, Supervivencia",
+        descripcionLarga: "«Me llamo... Maria», dice la mujer, sonriendo. Su rostro, su voz... Es igual que ella.",
+        imagen: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2124490/capsule_616x353.jpg?t=1744248682",
+        descarga: "https://playpaste.net/?v=jPjG",
+                requisitos: {
+        minimo: [
+        "Requiere un procesador y un sistema operativo de 64 bits",
+        "SO: Windows 10 x64",
+        "PROCESADOR: Intel Core i7-6700K | AMD Ryzen 5 3600",
+        "MEMORIA: 16 GB de RAM",
+        "GRÁFICOS: NVIDIA® GeForce® GTX 1070 Ti or AMD Radeon™ RX 5700 or Intel® Arc™ A750",
+        "DIRECTX: Versión 12",
+        "ALMACENAMIENTO: 50 GB de espacio disponible",
+        ],
+        recomendado: [
+        "Requiere un procesador y un sistema operativo de 64 bits",
+        "SO: Windows 10 x64",
+        "PROCESADOR: Intel Core i7-8700K | AMD Ryzen 5 3600X",
+        "MEMORIA: 16 GB de RAM",
+        "GRÁFICOS: NVIDIA® GeForce® 2080RTX or AMD Radeon™ 6800XT",
+        "DIRECTX: Versión 12",
+        "ALMACENAMIENTO: 50 GB de espacio disponible",
+        
+        ]
+        },
+        contenido: `
+        Descargar Silent Hill 2 Deluxe Edition Juego para PC en Español
+
+        DLC:
+        - Mira the Dog Mask
+        - Pyramid Head Mask
+        - Soundtrack
+
+        Updates:
+        - Update v1.05 (Se descarga e instala por separado)
+        - Update v1.06 (Se descarga e instala por separado)
+        - Update v1.07 (Se descarga e instala por separado)
+`,
+
+
+        tutoriales: [
+        {
+        titulo: "Cómo instalar el juego",
+        link: "https://playpaste.net/?v=jPjl"
+        },
+        
+        ]
+        
+    },
+    {
+        id: 3,
+        nombre: "the last of us 2",
+        descripcion: "supervivencia, post apocaliptico",
+        descripcionLarga: "Descargar The Last of US Part II Remastered para PC en Español.",
+        imagen: "https://assetsio.gnwcdn.com/The-Last-of-Us-Part-II-Remastered.png?width=690&quality=85&format=jpg&dpr=3&auto=webp",
+        descarga: "https://playpaste.net/?v=jPjH",
+                requisitos: {
+        minimo: [
+        "Requiere un procesador y un sistema operativo de 64 bits",
+        "SO: Windows 10/11 64-bit (version 1909 or higher)",
+        "PROCESADOR: Intel Core i3-8100, AMD Ryzen 3 1300X",
+        "MEMORIA: 16 GB de RAM",
+        "GRÁFICOS: NVIDIA GeForce GTX 1650, AMD Radeon RX 5500XT",
+        "ALMACENAMIENTO: 150 GB de espacio disponible",
+        "NOTAS ADICIONALES: SSD Required",
+        ],
+        recomendado: [
+        "Requiere un procesador y un sistema operativo de 64 bits",
+        "SO: Windows 10/11 64-bit (version 1909 or higher)",
+        "PROCESADOR: Intel Core i5-8600, AMD Ryzen 5 3600",
+        "MEMORIA: 16 GB de RAM",
+        "GRÁFICOS: NVIDIA GeForce RTX 3060, AMD Radeon RX 5700",
+        "ALMACENAMIENTO: 150 GB de espacio disponible",
+        "NOTAS ADICIONALES: SSD Required",
+        ]
+        },
+        contenido:`
+        Descargar The Last of US Part 2 Juego para PC en Español v1.6.10721; included DLC: Pre-purchase Entitlements, Soundtrack (mp3)
+        `,
+
+        tutoriales: [
+        {
+        titulo: "Cómo instalar el juego",
+        link: "https://playpaste.net/?v=jPjL"
+        },
+        
+        ]
+        
+    }
+];
